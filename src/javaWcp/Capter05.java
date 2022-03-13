436644c774a2176fe8235522936a0eb9905d486c
shunsuke <101243634+MatsuhashiShunsuke@users.noreply.github.com>
2022-03-13 22:12:38 +0900

    テスト

---
+++ src/javaWcp/Capter05.java
@@ -0,0 +1,13 @@
+package javaWcp;
+
+public class Capter05 {
+	public static void main(String[]args) {
+		String str="まつはし　しゅんすけ";
+		System.out.println(str);
+		str="やればできる!";
+		System.out.println(str);
+		
+		final int luckyNumber=6;
+		System.out.println(luckyNumber);
+	}
+}
